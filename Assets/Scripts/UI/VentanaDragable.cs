76c108fc94bd1f2553f8118feba46872a2d0d519
ElErizio <theericyea100@hotmail.com>
2024-09-13 00:22:19 -0600

    Publicación del Proyecto
    Proyecto para clase de Juegos en redes

---
+++ Assets/Scripts/UI/VentanaDragable.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class VentanaDragable : MonoBehaviour, IBeginDragHandler, IDragHandler
+{
+    public Vector3 offset;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
